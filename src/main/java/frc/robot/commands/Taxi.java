84b9ea6ca61032485a8966b4c036e2f0f94009f9
KelvinBM <kelvinmejia182@gmail.com>
2022-03-16 15:49:45 -0400

    fixed taxi

--- src/main/java/frc/robot/commands/Taxi.java
+++ src/main/java/frc/robot/commands/Taxi.java
@@ -4,32 +4,42 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
 import frc.robot.subsystems.DriveTrain;
 
 public class Taxi extends CommandBase {
 
   private DriveTrain driveTrain;
   private double speed;
+  private double time;
 
   /** Creates a new Taxi. */
-  public Taxi(DriveTrain driveTrain, double speed) {
+  public Taxi(DriveTrain driveTrain, double speed, double time) {
     this.driveTrain = driveTrain;
     this.speed = speed;
+    this.time = time;
     
     addRequirements(driveTrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    //driveTrain.arcadeDrive(speed);
+    
+      time = Timer.getFPGATimestamp();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
-    driveTrain.arcadeDrive(speed);
-    
+    while(time >= 0){
+      driveTrain.arcadeDrive(speed);
+      time--; 
+    }
+    driveTrain.motorStop(0);
   }
 
   // Called once the command ends or is interrupted.
